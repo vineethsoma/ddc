d5e9a9a3f245a529b6e0eabec5dedecd40ecc9c7
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-26 19:11:52 -0400

    added default values for dates

--- libs/scheduler/src/appointment/appointment.ts
+++ libs/scheduler/src/appointment/appointment.ts
@@ -1,6 +1,12 @@
 export class Appointment {
+    id?: string; 
     name: string; 
     phone: string; 
     startTime: Date;
-    endTime: Date; 
+    endTime: Date;
+
+    constructor() {
+        this.startTime = new Date();
+        this.endTime = new Date();
+    }
 }
