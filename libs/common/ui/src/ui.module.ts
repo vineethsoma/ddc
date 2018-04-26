01e513522faa9f688e50a4c39b96113ea01cc97d
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-26 08:59:52 -0400

    adjusted layout for shell container

--- libs/common/ui/src/ui.module.ts
+++ libs/common/ui/src/ui.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuButtonComponent } from './menu-button/menu-button.component';
-import { MatButtonModule } from "@angular/material";
+import { MatButtonModule, MatCardModule } from "@angular/material";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 @NgModule({
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [MenuButtonComponent, HeaderComponent, FooterComponent],
   declarations: [MenuButtonComponent, HeaderComponent, FooterComponent]
