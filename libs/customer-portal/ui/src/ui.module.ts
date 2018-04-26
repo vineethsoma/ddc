95b523bcc368f50388fa8b14cf53cc392ebe15d7
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-26 06:44:24 -0400

    common ui module added

--- libs/customer-portal/ui/src/ui.module.ts
+++ libs/customer-portal/ui/src/ui.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const uiRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [HomeComponent]
 })
 export class UiModule {}
