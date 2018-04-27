ec254cf5c2c423b51cf4647b921af8168f59acb5
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-27 06:46:19 -0400

    add routing module

--- libs/scheduler/src/scheduler.module.ts
+++ libs/scheduler/src/scheduler.module.ts
@@ -4,12 +4,16 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers as schedulerReducer } from './+state/scheduler.reducer';
 import { SchedulerEffects } from './+state/scheduler.effects';
+import { ScheduleModule } from "primeng/schedule";
+import { SchedulerRoutingModule } from '@ddc/scheduler/src/scheduler-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SchedulerRoutingModule,
     StoreModule.forFeature('scheduler', schedulerReducer),
-    EffectsModule.forFeature([SchedulerEffects])
+    EffectsModule.forFeature([SchedulerEffects]),
+    ScheduleModule
   ],
   providers: [SchedulerEffects]
 })
